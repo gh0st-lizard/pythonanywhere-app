99c2085bafb5b951c487f7299988610208d387f4
gh0st-lizard <gh0st_lizard@proton.me>
2025-12-11 16:06:07 -0500

    add test file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 
 app = Flask(__name__)
 
@@ -9,6 +9,13 @@ def index():
 @app.route("/api/health")
 def health():
     return jsonify(status="ok")
+
+@app.route('/<filename>')
+def download_file(filename):
+ # The directory where the files are  located
+ directory = 'files'
+ # Flask's send_from_directory to send the file to the client
+ return send_from_directory(directory, filename, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
